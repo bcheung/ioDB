86b387f4150d3e8c45b035bb9ea684e6afc1a74e
Brian Cheung <bcheung.dev@gmail.com>
2019-02-24 13:32:50 -0600

    added component templates

---
+++ web/src/components/NewComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const NewComponent = (props) => {
+    return (
+      <div style={styles.divStyle} className="NewComponent">
+        <header className="NewComponent-header"></header>
+      </div>
+    );
+}
+
+const styles = {
+  divStyle: {
+  }
+};
+
+export {NewComponent};
