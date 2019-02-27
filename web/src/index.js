0fef4e66d08712a0dede1f31e84def0e26a2c4fa
Brian Cheung <bcheung.dev@gmail.com>
2019-02-27 17:13:43 -0600

    basic git stats

--- web/src/index.js
+++ web/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages//App/App';
 import * as serviceWorker from './serviceWorker';
+import AboutPage from './pages/About/AboutPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AboutPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
