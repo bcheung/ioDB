cd2b0faff63cc81a8b35a0aed58bbc6fab94cbca
David Terral <david.terral@utexas.edu>
2019-02-27 13:39:44 -0600

    added 3 static location pages

---
+++ web/src/pages/Location Pages/location3.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'location3.css';
+
+class Page extends Component {
+  render() {
+    return (
+      <div className="Page">
+        <header className="Page-header"></header>
+      </div>
+    );
+  }
+}
+
+export default Page;
