c765302585f9efb8c6ca740acd7dc4bf52ab4e1b
Brian Cheung <bcheung.dev@gmail.com>
2019-02-27 16:35:13 -0600

    refactor location pages

--- web/src/index.js
+++ web/src/index.js
@@ -5,20 +5,26 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 // import App from './pages/App/App';
 import HomePage from './pages/Home/HomePage'
+import IndustryPage from './pages/IndustryMain/IndustryPage';
 import OccupationPage from './pages/OccupationMain/OccupationPage';
 import ChemicalEngineers from './pages/ChemicalEngineers/ChemicalEngineers';
 import DentistsGen from './pages/DentistsGen/DentistsGen';
 import SoftwareDevSystems from './pages/SoftwareDevSystems/SoftwareDevSystems';
 import LocationPage from './pages/LocationMain/LocationPage';
+import Austin from './pages/Austin/Austin';
+import SanJose from './pages/SanJose/SanJose';
+import NewYork from './pages/NewYork/NewYork';
 
 const routing = (
   <Router>
     <div>
-		<Route path="/" component={HomePage} />
-			{/* <Route path="/industry" component={IndustryPage} />
-			<Route path="/industry/Austin" component={ChemicalEngineers} />
-      <Route path="/industry/SanJose" component={DentistsGen} />
-			<Route path="/industry/NewYork" component={SoftwareDevSystems} /> */}
+			<Route path="/" component={HomePage} />
+			<Route path="/about" component={HomePage} />
+			
+			<Route path="/industry" component={IndustryPage} />
+			{/* <Route path="/industry/Dentists" component={Dentists} />
+      <Route path="/industry/CEManufacturing" component={CEManufacturing} />
+			<Route path="/industry/Research" component={Research} /> */}
 
       <Route path="/occupation" component={OccupationPage} />
       <Route path="/occupation/ChemicalEngineers" component={ChemicalEngineers} />
@@ -26,19 +32,15 @@ const routing = (
 			<Route path="/occupation/SoftwareDevSystems" component={SoftwareDevSystems} />
 
 			<Route path="/location" component={LocationPage} />
-			<Route path="/location/Austin" component={ChemicalEngineers} />
-      <Route path="/location/SanJose" component={DentistsGen} />
+			<Route path="/location/Austin" component={Austin} />
+      <Route path="/location/SanJose" component={SanJose} />
+      <Route path="/location/SanJose" component={NewYork} />
 			
     </div>
   </Router>
 )
 
 ReactDOM.render(routing, document.getElementById('root'));
-
-<<<<<<< HEAD
-=======
-ReactDOM.render(<Location1 />, document.getElementById('root'));
->>>>>>> e0ebf07ce58556cf3527b6afb41ccb5d0c3b0a7b
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- web/src/pages/SanJose/SanJose.js
+++ web/src/pages/SanJose/SanJose.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import 'location3.css';
+import './location2.css';
 
-class NewYork extends Component {
+class SanJose extends Component {
   render() {
     return (
       <div className="Page">
         <header className="Page-header"></header>
         <body>
 
-        <h2>New York-Newark-Jersey City, NY-NJ-PA</h2>
-        <p>Annual Mean: $62,870</p>
+        <h2>San Jose-Sunnyvale-Santa Clara, CA</h2>
+        <p>Annual Mean: $77,180</p>
         <p>Occupations</p>
 
         <table style="width:100%">
@@ -24,51 +24,51 @@ class NewYork extends Component {
             <tr>
             <td>Office and Administrative Support Occupations</td>
             <td>430000</td>
-            <td>1522930</td>
-            <td>43870</td>
-            <td>1.06</td>
+            <td>135970</td>
+            <td>48050</td>
+            <td>0.81</td>
             </tr>
             
             <tr>
             <td>Sales and Related Occupations</td>
             <td>410000</td>
-            <td>954440</td>
-            <td>53740</td>
-            <td>1.01</td>
+            <td>99190</td>
+            <td>55290</td>
+            <td>0.89</td>
             </tr>
             
             <tr>
             <td>Food Preparation and Serving Related Occupations</td>
             <td>350000</td>
-            <td>713170</td>
-            <td>29000</td>
-            <td>0.83</td>
+            <td>82000</td>
+            <td>29730</td>
+            <td>0.81</td>
             </tr>
             
             <tr>
-            <td>Business and Financial Operations Occupations</td>
-            <td>130000</td>
-            <td>593610</td>
-            <td>97410</td>
-            <td>1.22</td>
+            <td>Management Occupations</td>
+            <td>110000</td>
+            <td>84760</td>
+            <td>163380</td>
+            <td>1.52</td>
             </tr>
             
             
             <tr>
-            <td>Food Preparation and Serving Related Occupations</td>
-            <td>250000</td>
-            <td>667590</td>
-            <td>68140</td>
-            <td>1.17</td>
+            <td>Computer and Mathematical Occupations</td>
+            <td>150000</td>
+            <td>132140</td>
+            <td>122900</td>
+            <td>4.06</td>
             </tr>
             
             
-          </table>
+        </table>
 
         </body>
-      </div>
+        </div>
     );
   }
 }
 
-export default NewYork;
+export default SanJose;
