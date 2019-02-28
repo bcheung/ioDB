b2e565b128dce8f9606ab326ce059cc6a43a83ae
Cooper Travis <ctravis096@gmail.com>
2019-02-28 17:33:26 -0600

    Scraper API is written

---
+++ scraper/occupation.py
@@ -0,0 +1,15 @@
+
+
+class Occupation:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.description = ""
+        self.industries = []
+        self.aMeans = []
+        self.aMedians = []
+        self.totalEmployed = 0
+        self.aMean = 0
+        self.aMedian = 0
+
+    
