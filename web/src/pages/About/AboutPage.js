aa7b918d8b095fee3bc64f7a9edc7b55c9911b5c
brookepaxman <brooke.paxman@utexas.edu>
2019-03-01 18:36:05 -0600

    version-control

--- web/src/pages/About/AboutPage.js
+++ web/src/pages/About/AboutPage.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import "./about-page.css";
 import Brooke from './brooke.jpg';
-<<<<<<< HEAD
 //import Cooper from './cooper.jpg';
 //import Brian from './brian.jpg';
 //import Sean from './sean.jpg';
-=======
 import Cooper from './cooper.png';
 import Brian from './brian.jpeg';
 import Sean from './sean.jpeg';
->>>>>>> b3cbb89... Updated About Page
 //import David from './david.jpg';
 //import Johnny from './johnny.jpg';
 import logo from './default.jpg';
@@ -20,14 +17,6 @@ class AboutPage extends Component {
     super();
     this.state = {
       branches: [],
-<<<<<<< HEAD
-      issues: 0,
-      commitNum: 0,
-      commits: [0,0,0,0,0,0],
-      urlD: []
-		};
-  }
-=======
       issues: [0,0,0,0,0,0],
       issueNum: 0,
       commits: [0,0,0,0,0,0],
@@ -37,71 +26,42 @@ class AboutPage extends Component {
   }
 
   commitCount = 0;
->>>>>>> b3cbb89... Updated About Page
   
   people = [
     {
       name: "Cooper Travis",
       github: "CooperTravis",
       eventURL: "",
-<<<<<<< HEAD
-=======
       issues: 0
->>>>>>> b3cbb89... Updated About Page
     },
     {
       name: "Brian Cheung",
       github: "bcheung",
       eventURL: "",
-<<<<<<< HEAD
-=======
       issues: 0
->>>>>>> b3cbb89... Updated About Page
     },
     {
       name: "Brooke Paxman",
       github: "brookepaxman",
       eventURL: "",
-<<<<<<< HEAD
-=======
       issues: 0
->>>>>>> b3cbb89... Updated About Page
     },
     {
       name: "Sean Wang",
       github: "wang-sz",
       eventURL: "",
-<<<<<<< HEAD
-=======
       issues: 0
->>>>>>> b3cbb89... Updated About Page
     },
     {
       name: "David Terral",
       github: "dterral504",
       eventURL: "",
-<<<<<<< HEAD
-=======
       issues: 0
->>>>>>> b3cbb89... Updated About Page
     },
     {
       name: "Johnny Gabriel",
       github: "jmgabriel96",
       eventURL: "",
-<<<<<<< HEAD
-    }
-  ];
-	
-	
-  
-  componentDidMount() {
-    this.fetchIssues();
-    this.fetchBranches();
-    this.obtainURLs();
-    this.fetchCommits();
-    this.fetchCommitNum();
-=======
       issues: 0
     }
   ];
@@ -112,7 +72,6 @@ class AboutPage extends Component {
     this.fetchBranches();
     this.obtainURLs();
     this.fetchCommits();
->>>>>>> b3cbb89... Updated About Page
   }
 
   obtainURLs(){
@@ -142,43 +101,6 @@ class AboutPage extends Component {
       })
     })
 
-<<<<<<< HEAD
-  }
-
-  fetchCommits() {
-    var q;
-    var url;
-    for(q = 0; q < this.people.length; q++){
-      url = this.people[q].eventURL;
-      this.fetchURL(url,q);
-    }
-  }
-
-  fetchURL(url,q){
-    if(q < this.people.length){
-      var commitNum = 0;
-     fetch(url)
-        .then(results => results.json())
-        .then(data => {
-          let dataCopy = this.state.commits;
-          var j;
-          for(j = 0; j < data.length; j++){
-            if(data[j].type == "PushEvent"){
-              if(data[j].repo.name == "bcheung/ioDB"){
-                dataCopy[q] += 1;
-              }
-            }
-          }
-          this.setState({
-            commits:dataCopy
-          })
-        })
-    }
-  }
-
-
-  fetchIssues() {
-=======
   }
 
   fetchCommits() {
@@ -217,7 +139,6 @@ class AboutPage extends Component {
   fetchTotalIssues() {
     var githubName;
     var index = 0;
->>>>>>> b3cbb89... Updated About Page
     const url = "https://api.github.com/repos/bcheung/ioDB/issues";
     fetch(url)
         .then(results => results.json())
@@ -262,43 +183,6 @@ class AboutPage extends Component {
   }
 
   render() {
-<<<<<<< HEAD
-    const { issues,commitNum } = this.state;
-    return(
-      <div>
-        <h1>About ioDB</h1>
-        <p>We seek to provide a more intuitive and less cluttered interface 
-          that will focus on key metrics of US occupations such as location, occupation category,
-          and industry that people look for while researching occupations and industries in the US. 
-          ioDB provides its users with an easy to navigate UI to help users gain a more meaningful 
-           understanding of all the available information.</p>
-        <p>Group Name: Team Amethyst</p>
-        <ul>
-          <h2><li>{this.people[0].name+ ": "+this.people[0].github}</li></h2> 
-            <img src={logo} id="Cooper" width="200" height = "200" alt = "Cooper Travis"></img>
-            <ul><li>{"Commits: " +this.state.commits[0]}</li></ul>
-          <h2><li>{this.people[1].name+ ": "+this.people[1].github}</li></h2> 
-          <img src={logo} id="Brian" width="200" height = "200" alt = "Brian Cheung"></img>
-            <ul><li>{"Commits: " +this.state.commits[1]}</li></ul>
-          <h2><li>{this.people[2].name+ ": "+this.people[2].github}</li></h2>
-            <img src={Brooke} id="Brooke" width="200" height = "200" alt = "Brooke Paxman"></img>
-            <ul><li>{"Commits: " +this.state.commits[2]}</li></ul> 
-          <h2><li>{this.people[3].name+ ": "+this.people[3].github}</li></h2> 
-            <img src={logo} id="Sean" width="200" height = "200" alt = "Sean Wang"></img>
-            <ul><li>{"Commits: " +this.state.commits[3]}</li></ul>
-          <h2><li>{this.people[4].name+ ": "+this.people[4].github}</li></h2>
-            <img src={logo} id="David" width="200" height = "200" alt = "David Terral"></img> 
-            <ul><li>{"Commits: " +this.state.commits[4]}</li></ul>
-          <h2><li>{this.people[5].name+ ": "+this.people[5].github}</li></h2>
-          <img src={logo} id="Johnny" width="200" height = "200" alt = "Johnny Gabriel"></img> 
-            <ul><li>{"Commits: " +this.state.commits[5]}</li></ul>
-        </ul>    
-        <p>Total Stats:</p>
-        <ul>
-          <li>Issues: {issues}</li>
-          <li>Commits: {commitNum}</li>
-        </ul>
-=======
     const { issueNum,commitNum } = this.state;
     return(
       <div>
@@ -413,7 +297,6 @@ class AboutPage extends Component {
         </ul>
         <h3>Data:</h3>
         <h3>Tools:</h3>
->>>>>>> b3cbb89... Updated About Page
         <p>The following is a link to our Github:
         <a href="https://github.com/bcheung/ioDB/">Github Repo</a>
         </p>
