d26b245818c9d5cc3b6ebba74e05a1f4a969ccdd
Brian Cheung <bcheung.dev@gmail.com>
2019-03-21 16:41:17 -0700

    general models

--- api/create_tables.py
+++ api/create_tables.py
@@ -1,6 +1,5 @@
 from main import db
 
-
 if __name__ == '__main__':
     print('Creating all database tables...')
     db.create_all()
--- api/main.py
+++ api/main.py
@@ -4,9 +4,9 @@ import os
 import socket
 
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
 
+from models import db, Visit
 
 app = Flask(__name__)
 
@@ -24,17 +24,8 @@ def is_ipv6(addr):
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-
-class Visit(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime())
-    user_ip = db.Column(db.String(46))
-
-    def __init__(self, timestamp, user_ip):
-        self.timestamp = timestamp
-        self.user_ip = user_ip
+db.app = app
+db.init_app(app)
 
 
 @app.route('/api/')
