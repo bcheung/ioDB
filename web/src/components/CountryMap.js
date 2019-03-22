c68024d43e6a790fc280e49bb8f52dfa493d3133
Sean Wang <sean.wang@utexas.edu>
2019-03-22 02:40:20 -0500

    added US map

---
+++ web/src/components/CountryMap.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography
+} from "react-simple-maps"
+import { geoAlbersUsa } from "d3-geo";
+import geoData from '../static/usa-states.json'
+
+const wrapperStyles = {
+  width: "100%",
+  maxWidth: 980,
+  margin: "0 auto",
+}
+
+class CountryMap extends Component {
+  render() {
+    return (
+      <div className="map-container">
+        <ComposableMap
+          projection={geoAlbersUsa}
+          projectionConfig={{ scale: 1000 }}
+          width={980}
+          height={551}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          >
+          <ZoomableGroup center={[ -97, 40 ]} disablePanning zoom={1}>
+            <Geographies geography={geoData} disableOptimization>
+              {(geographies, projection) =>
+                geographies.map((geography, i) =>
+                <Geography
+                  key={i}
+                  geography={geography}
+                  projection={projection}
+                  style={{
+                    default: {
+                      fill: "#ECEFF1",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#CFD8DC",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#FF5722",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                  }}
+                />
+              )}
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    );
+  }
+}
+
+export default CountryMap;
