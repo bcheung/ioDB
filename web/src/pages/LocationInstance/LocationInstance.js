28d594b749539e8391d2390df974886d61058bf4
Sean Wang <sean.wang@utexas.edu>
2019-03-23 16:08:39 -0500

    instance template

--- web/src/pages/LocationInstance/LocationInstance.js
+++ web/src/pages/LocationInstance/LocationInstance.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-// import './Page.css';
-import { button } from '@coreui/react';
+import CountryMap from '../../components/CountryMap';
 
-class Page extends Component {
+class LocationInstance extends Component {
     render() {
         return (
-            <div className="Page">
-                <header className="Page-header" />
-                <button type="button" className="btn btn-pill btn-primary">
-                    Button
-                </button>
+            <div>
+                <CountryMap />
             </div>
         );
     }
 }
 
-export default Page;
+export default LocationInstance;
--- web/src/pages/LocationInstance/LocationInstance.test.js
+++ web/src/pages/LocationInstance/LocationInstance.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Page from './Page';
+import LocationInstance from './LocationInstance';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Page />, div);
+    ReactDOM.render(<LocationInstance />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
