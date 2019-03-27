a8f45a1ae18124b68145e707286aa383c3f124de
brookepaxman <brooke.paxman@utexas.edu>
2019-03-27 16:51:45 -0500

    mapbox state fill layer

--- web/src/pages/detail/ChemicalEngineers/ChemicalEngineers.js
+++ web/src/pages/detail/ChemicalEngineers/ChemicalEngineers.js
@@ -88,6 +88,116 @@ const data2 = {
     }
 };
 
+const occupation = {
+    annual_10: 48220,
+    annual_25: 69880,
+    annual_75: 149410,
+    annual_90: 208000,
+    annual_mean: 119910,
+    annual_median: 102590,
+    description: '',
+    hourly_10: 23.19,
+    hourly_25: 33.6,
+    hourly_75: 71.83,
+    hourly_90: 100.0,
+    hourly_mean: 57.65,
+    hourly_median: 49.32,
+    id: '11-0000',
+    occupations_detailed: [
+        '11-1011',
+        '11-1021',
+        '11-1031',
+        '11-2011',
+        '11-2021',
+        '11-2022',
+        '11-2031',
+        '11-3011',
+        '11-3021',
+        '11-3031',
+        '11-3051',
+        '11-3061',
+        '11-3071',
+        '11-3111',
+        '11-3121',
+        '11-3131',
+        '11-9013',
+        '11-9021',
+        '11-9031',
+        '11-9032',
+        '11-9033',
+        '11-9039',
+        '11-9041',
+        '11-9051',
+        '11-9061',
+        '11-9071',
+        '11-9081',
+        '11-9111',
+        '11-9121',
+        '11-9131',
+        '11-9141',
+        '11-9151',
+        '11-9161',
+        '11-9199'
+    ],
+    title: 'Management Occupations',
+    total_employment: 7280330
+};
+
+const states = [
+    { STATE_ID: '01', unemployment: 13.17 },
+    { STATE_ID: '02', unemployment: 9.5 },
+    { STATE_ID: '04', unemployment: 12.15 },
+    { STATE_ID: '05', unemployment: 8.99 },
+    { STATE_ID: '06', unemployment: 11.83 },
+    { STATE_ID: '08', unemployment: 7.52 },
+    { STATE_ID: '09', unemployment: 6.44 },
+    { STATE_ID: '10', unemployment: 5.17 },
+    { STATE_ID: '12', unemployment: 9.67 },
+    { STATE_ID: '13', unemployment: 10.64 },
+    { STATE_ID: '15', unemployment: 12.38 },
+    { STATE_ID: '16', unemployment: 10.13 },
+    { STATE_ID: '17', unemployment: 9.58 },
+    { STATE_ID: '18', unemployment: 10.63 },
+    { STATE_ID: '19', unemployment: 8.09 },
+    { STATE_ID: '20', unemployment: 5.93 },
+    { STATE_ID: '21', unemployment: 9.86 },
+    { STATE_ID: '22', unemployment: 9.81 },
+    { STATE_ID: '23', unemployment: 7.82 },
+    { STATE_ID: '24', unemployment: 8.35 },
+    { STATE_ID: '25', unemployment: 9.1 },
+    { STATE_ID: '26', unemployment: 10.69 },
+    { STATE_ID: '27', unemployment: 11.53 },
+    { STATE_ID: '28', unemployment: 9.29 },
+    { STATE_ID: '29', unemployment: 9.94 },
+    { STATE_ID: '30', unemployment: 9.29 },
+    { STATE_ID: '31', unemployment: 5.45 },
+    { STATE_ID: '32', unemployment: 4.21 },
+    { STATE_ID: '33', unemployment: 4.27 },
+    { STATE_ID: '34', unemployment: 4.09 },
+    { STATE_ID: '35', unemployment: 7.83 },
+    { STATE_ID: '36', unemployment: 8.01 },
+    { STATE_ID: '37', unemployment: 9.34 },
+    { STATE_ID: '38', unemployment: 11.23 },
+    { STATE_ID: '39', unemployment: 7.08 },
+    { STATE_ID: '40', unemployment: 11.22 },
+    { STATE_ID: '41', unemployment: 6.2 },
+    { STATE_ID: '42', unemployment: 9.11 },
+    { STATE_ID: '44', unemployment: 10.42 },
+    { STATE_ID: '45', unemployment: 8.89 },
+    { STATE_ID: '46', unemployment: 11.03 },
+    { STATE_ID: '47', unemployment: 7.35 },
+    { STATE_ID: '48', unemployment: 8.92 },
+    { STATE_ID: '49', unemployment: 7.65 },
+    { STATE_ID: '50', unemployment: 8.01 },
+    { STATE_ID: '51', unemployment: 7.62 },
+    { STATE_ID: '53', unemployment: 7.77 },
+    { STATE_ID: '54', unemployment: 8.49 },
+    { STATE_ID: '55', unemployment: 9.42 },
+    { STATE_ID: '56', unemployment: 7.59 }
+];
+
+const expression = ['match', ['get', 'STATE_ID']];
+
 class ChemicalEngineers extends Component {
     constructor(props: Props) {
         super(props);
@@ -99,28 +209,42 @@ class ChemicalEngineers extends Component {
     componentDidMount() {
         this.map = new mapboxgl.Map({
             container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v9',
+            style: 'mapbox://styles/mapbox/light-v10',
             center: [-96.5, 40],
             zoom: 2.5
         });
 
         this.map.on('load', () => {
             this.map.addSource('states', {
-                type: 'geojson',
-                data
+                type: 'vector',
+                url: 'mapbox://mapbox.us_census_states_2015'
             });
 
+            const maxValue = 13;
+            // Calculate color
+            states.forEach(function(row) {
+                const green = (row.unemployment / maxValue) * 255;
+                const color = `rgba(${0}, ${green}, ${0}, 1)`;
+                expression.push(row.STATE_ID, color);
+            });
+
+            expression.push('rgba(0,0,0,0)');
+
+            // Add layer from the vector tile source with data-driven style
             this.map.addLayer(
                 {
-                    id: 'countries',
+                    id: 'states-join',
                     type: 'fill',
-                    source: 'states'
+                    source: 'states',
+                    'source-layer': 'states',
+                    paint: {
+                        'fill-color': expression
+                    }
                 },
-                'country-label-lg'
-            ); // ID metches `mapbox/streets-v9`
-
-            this.setFill();
+                'waterway-label'
+            );
         });
+        console.log(this.map);
     }
 
     componentDidUpdate() {
@@ -134,6 +258,33 @@ class ChemicalEngineers extends Component {
             stops
         });
     }
+
+    // async fetchOccupationData() {
+    //     let issuesTotal = 0;
+    //     const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+    //     const url = `${proxyurl}http://www.iodb.info/api/joined_instance/state_occ_major/occupations_major/11-000`;
+    //     // ${11-000}`;
+    //     // this.props.id
+
+    //     // without async await
+    //     // fetch(url)
+    //     //   .then(response => response.json())
+    //     //   .then(data => {
+    //     //     data.forEach(issue => {
+    //     //       const username = issue.user;
+    //     //       this.contributorStats[username].issues++;
+    //     //       issuesTotal++;
+    //     //     });
+    //     //   });
+
+    //     const response = await fetch(url);
+    //     const format = await response.json();
+    //     format.forEach(state => {
+    //         console.log(format);
+    //         const statequotient = state.loc_quotient;
+    //         issuesTotal++;
+    //     });
+    // }
 
     map;
 
