b4f0a351a44b7460a9d81d6f1e61baf8d7e62ff8
jmgabriel96 <jmgabriel@utexas.edu>
2019-04-10 18:57:37 -0500

    Fixed HomePage and NewPage Imports

--- web/src/pages/New Page/NewPage.test.js
+++ web/src/pages/New Page/NewPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Page from './Page';
+import NewPage from './NewPage';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Page />, div);
+    ReactDOM.render(<NewPage />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
