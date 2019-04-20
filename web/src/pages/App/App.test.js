d2b734c2c2ddeee91dfcf54b7797aec54b2a076a
Sean Wang <sean.wang@utexas.edu>
2019-04-20 18:53:05 -0500

    added some tests and fixed some

--- web/src/pages/App/App.test.js
+++ web/src/pages/App/App.test.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+    Map: () => ({})
+    // mapboxgl: {
+    //     accessToken: '',
+    //     Map: () => ({}),
+    // },
+}));
+
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
--- web/src/pages/OccupationInstance/OccupationInstancePage.test.js
+++ web/src/pages/OccupationInstance/OccupationInstancePage.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import OccupationInstancePage from './OccupationInstancePage';
+import OccupationInstancePage, { getMaxLocQuotient } from './OccupationInstancePage';
+import { fetchInstanceData, fetchJoinedInstanceData } from '../../fetchAPI';
+import { shallow } from 'enzyme';
+
 
 jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
     Map: () => ({})
@@ -9,6 +12,13 @@ jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
     //     Map: () => ({}),
     // },
 }));
+
+const match = {
+    params: {
+        tablename: "occupations_major",
+        id: "11-000",
+    }
+}
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -21,3 +31,10 @@ it('renders without crashing', () => {
     ReactDOM.render(<OccupationInstancePage match={match} />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+it('get max location quotient', () => {
+    const wrapper = shallow(<OccupationInstancePage match={match}/>).instance();
+    const locData = fetchJoinedInstanceData("occupations_major", '11-000');
+    const maxLocQ = getMaxLocQuotient(locData);
+    expect(maxLocQ).toEqual(null);
+})
