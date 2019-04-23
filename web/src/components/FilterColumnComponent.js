451aa4e2722087464bc71b54346c0f4238601b82
Brian Cheung <bcheung.dev@gmail.com>
2019-04-22 23:41:44 -0500

    styling

--- web/src/components/FilterColumnComponent.js
+++ web/src/components/FilterColumnComponent.js
@@ -23,31 +23,33 @@ class FilterColumnComponent extends Component {
         return (
             <Container>
                 <Row>
-                    <input
-                        type="number"
-                        value={filter ? filter.value.value : ''}
-                        onChange={event => {
-                            const val = {
-                                option: filter ? filter.value.option : 0,
-                                value: event.target.value === '' ? '' : Number(event.target.value)
-                            };
-                            onChange(val);
-                        }}
-                    />
-                </Row>
-                <Row>
                     <ButtonGroup>
-                        <Button color="light" onClick={() => this.handleOptionClick(1)} active={option === 1}>
+                        <Button onClick={() => this.handleOptionClick(1)} active={option === 1}>
                             ≥
                         </Button>
-                        <Button color="light" onClick={() => this.handleOptionClick(2)} active={option === 2}>
+                        <Button onClick={() => this.handleOptionClick(2)} active={option === 2}>
                             =
                         </Button>
-                        <Button color="light" onClick={() => this.handleOptionClick(3)} active={option === 3}>
+                        <Button onClick={() => this.handleOptionClick(3)} active={option === 3}>
                             ≤
                         </Button>
                     </ButtonGroup>
                 </Row>
+                {option !== 0 ? (
+                    <Row>
+                        <input
+                            type="number"
+                            value={filter ? filter.value.value : ''}
+                            onChange={event => {
+                                const val = {
+                                    option: filter ? filter.value.option : 0,
+                                    value: event.target.value === '' ? '' : Number(event.target.value)
+                                };
+                                onChange(val);
+                            }}
+                        />
+                    </Row>
+                ) : null}
             </Container>
         );
     }
